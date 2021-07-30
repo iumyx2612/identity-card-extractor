985b1835837ed68a22f8aa68430143688ada0661
Nguyen Mai <DeftNotTroanh@gmail.com>
2021-07-30 16:39:27 +0700

    Add main

---
+++ workspace/main.py
@@ -0,0 +1,7 @@
+from utillib.utils import *
+
+
+
+if __name__ == '__main__':
+    inspect_annotation_with_image("Data/CMT_content/Validation/Images/01_001.jpeg",
+                                  "Data/CMT_content/Validation/Annotations/01_001.xml")
